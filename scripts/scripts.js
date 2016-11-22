f8110d054c46ef026b409ddd9b399cc9f43a01b9
Nathalie Tate <nathalie.tate00@gmail.com>
2016-11-22 14:12:24 -0500

    Light (#27)
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * CSS
    
    * toggleTheme
    
    * syntax error
    
    * toggleTheme
    
    * updated help

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,7 @@
 	msg = {
 					"user" : "AceChat",
 					"command" : "SYSTEM",
-					"args" : [null, "Type /user &lt;username&gt; to log in<br>If you would like to join a nondefault server, type /server &lt;server&gt; first<br>The default server is "+ server],
+					"args" : [null, "Type /user &lt;username&gt; to log in<br>If you would like to join a non-default server, type /server &lt;server&gt; first<br>The default server is "+ server],
 					"timestamp" : new Date().getTime()/1000
 				};
 	
